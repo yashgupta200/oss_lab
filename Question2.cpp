46f70e0279b2a5a7dea7ba5194bfce941d345f37
YASH  GUPTA <158084812+yashgupta200@users.noreply.github.com>
2024-08-03 23:48:34 +0530

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+      sum+=arr[i];
+    }
+    float avg;
+    avg=(sum/n);
+    cout<<"the average of given number"<<avg;
+    return 0;
+}
